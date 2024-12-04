bdbce2969161e1ba252c63f4d6e77227c373f3d2
TheShepsMT <theshepsmt@gmail.com>
2024-12-04 16:00:07 -0500

    target

---
+++ target/generated-sources/annotations/com/tecsup/petclinic/mapper/PetMapperImpl.java
@@ -0,0 +1,77 @@
+package com.tecsup.petclinic.mapper;
+
+import com.tecsup.petclinic.domain.PetTO;
+import com.tecsup.petclinic.entities.Pet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-12-04T15:57:34-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+)
+@Component
+public class PetMapperImpl implements PetMapper {
+
+    @Override
+    public Pet toPet(PetTO petTO) {
+
+        Pet pet = new Pet();
+
+        if ( petTO != null ) {
+            pet.setBirthDate( stringToDate( petTO.getBirthDate() ) );
+            pet.setId( petTO.getId() );
+            pet.setName( petTO.getName() );
+            pet.setTypeId( petTO.getTypeId() );
+            pet.setOwnerId( petTO.getOwnerId() );
+        }
+
+        return pet;
+    }
+
+    @Override
+    public PetTO toPetTO(Pet pet) {
+
+        PetTO petTO = new PetTO();
+
+        if ( pet != null ) {
+            petTO.setBirthDate( dateToString( pet.getBirthDate() ) );
+            petTO.setId( pet.getId() );
+            petTO.setName( pet.getName() );
+            petTO.setTypeId( pet.getTypeId() );
+            petTO.setOwnerId( pet.getOwnerId() );
+        }
+
+        return petTO;
+    }
+
+    @Override
+    public List<PetTO> toPetTOList(List<Pet> petList) {
+        if ( petList == null ) {
+            return new ArrayList<PetTO>();
+        }
+
+        List<PetTO> list = new ArrayList<PetTO>( petList.size() );
+        for ( Pet pet : petList ) {
+            list.add( toPetTO( pet ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Pet> toPetList(List<PetTO> petTOList) {
+        if ( petTOList == null ) {
+            return new ArrayList<Pet>();
+        }
+
+        List<Pet> list = new ArrayList<Pet>( petTOList.size() );
+        for ( PetTO petTO : petTOList ) {
+            list.add( toPet( petTO ) );
+        }
+
+        return list;
+    }
+}
